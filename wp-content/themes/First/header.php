4aec57bc616b561208993a7415917fc33eb9f7e4
Levy <levyaraujo42@gmail.com>
2020-01-18 00:26:56 -0300

    theme
    download and update of theme

---
+++ wp-content/themes/First/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html class="no-js" <?php language_attributes();?>>
+
+	<head>
+
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+		<?php wp_head(); ?>
+	</head>
+<body>
+	<header>
+		<?php 
+			wp_nav_menu('primary');
+		?>
+	</header>		
+
